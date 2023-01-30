ecabf910ddb6cd0d673a3ca5a0ee6192bf9ba252
PyFaNNy <fanny.koob@gmail.com>
2023-01-30 17:27:34 +0300

    Add proxy

--- gRPC.Angular.Client/src/app/app.component.ts
+++ gRPC.Angular.Client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "./api.service";
+import {ListReply} from "./protos/generated/userCrud_pb";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gRPC.Angular.Client';
+
+  constructor(
+    private api: ApiService
+  ) {}
+
+  ngOnInit() {
+    this.api.list(1,1).then((data: ListReply) => {
+      console.log(data);
+    })
+  }
 }
